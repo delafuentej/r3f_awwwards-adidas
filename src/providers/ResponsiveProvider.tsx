49a85b17610cc3ca2293c03c3ede16790b66cf5c
Jesús de la Fuente <jfuentelucas@gmail.com>
2026-03-14 14:26:35 +0100

    responsive II

--- src/providers/ResponsiveProvider.tsx
+++ src/providers/ResponsiveProvider.tsx
@@ -12,8 +12,8 @@ export const ResponsiveProvider = ({ children }: ResponsiveProviderProps) => {
   const setDevice = useResponsive((state) => state.setDevice);
 
   const isMobile = useMediaQuery({ maxWidth: 767 });
-  const isTablet = useMediaQuery({ maxWidth: 1023 });
-  const isDesktop = useMediaQuery({ minWidth: 1024 });
+  const isTablet = useMediaQuery({ maxWidth: 1024 });
+  const isDesktop = useMediaQuery({ minWidth: 1025 });
 
   useEffect(() => {
     const scale = isMobile ? 0.6 : isTablet ? 0.8 : 1.0;
